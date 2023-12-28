951936ecc40c35e5a573173cb85237eea9d9fce5
DavidV1600 <david.voinescu@saguna.ro>
2023-12-28 17:48:14 +0200

    Adding TagController

--- Controllers/TagController.cs
+++ Controllers/TagController.cs
@@ -1,24 +1,104 @@
-﻿using Incerc_Site1.Models;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Incerc_Site1.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Incerc_Site1.Contexts;
 
 namespace Incerc_Site1.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
     public class TagController : ControllerBase
     {
-        private Incerc_Site1.Contexts.MyAppContext _Tags;
+        private readonly MyAppContext _context;
 
-        public TagController(Incerc_Site1.Contexts.MyAppContext tags)
+        public TagController(MyAppContext context)
         {
-            this._Tags = tags;
+            _context = context;
         }
 
+        // GET: api/Tag
         [HttpGet]
-        public List<Tag> GetCategoriiList()
+        public async Task<ActionResult<IEnumerable<Tag>>> GetTags()
         {
-            return _Tags.Tags.ToList();
+            return await _context.Tags.ToListAsync();
+        }
+
+        // GET: api/Tag/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Tag>> GetTagById(int id)
+        {
+            var tag = await _context.Tags.FindAsync(id);
+
+            if (tag == null)
+            {
+                return NotFound();
+            }
+
+            return tag;
+        }
+
+        // POST: api/Tag
+        [HttpPost]
+        public async Task<ActionResult<Tag>> CreateTag(Tag tag)
+        {
+            _context.Tags.Add(tag);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTagById), new { id = tag.TagId }, tag);
+        }
+
+        // PUT: api/Tag/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTag(int id, Tag tag)
+        {
+            if (id != tag.TagId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tag).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TagExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Tag/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTag(int id)
+        {
+            var tag = await _context.Tags.FindAsync(id);
+            if (tag == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tags.Remove(tag);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TagExists(int id)
+        {
+            return _context.Tags.Any(e => e.TagId == id);
         }
     }
 }
